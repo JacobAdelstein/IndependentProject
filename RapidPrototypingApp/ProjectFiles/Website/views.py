671bce1bed4315a7ed0632a7a48263859f941c62
Jacob <jacobadelstein@gmail.com>
2021-07-08 12:54:36 -0400

    Need to start fresh

--- RapidPrototypingApp/ProjectFiles/Website/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse('<H1> Home Page <H1>')
-
-def about(request):
-    return HttpResponse('<p> Meteor Prototypes<p>')
