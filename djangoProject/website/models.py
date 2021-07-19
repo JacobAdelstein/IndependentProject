25ae3c61634becfc055754e39572a90347772cc8
Jacob <jacobadelstein@gmail.com>
2021-07-19 13:42:08 -0400

    Create database model for project description
    Added description models to views.py

--- djangoProject/website/models.py
+++ djangoProject/website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+class Description(models.Model):
+    content = models.TextField()
+    date_posted=models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-# Create your models here.
+    def __str__(self):
+        return self.content
+
