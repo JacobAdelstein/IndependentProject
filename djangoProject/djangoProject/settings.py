be68e7a9ffadf258cb57d19fbf367a4df112176a
Jacob <jacobadelstein@gmail.com>
2021-07-20 17:26:33 -0400

    Profiles are now created. CSS styling applied to profile page. Profile pictures work now. Default profile picture added. Signal created in order to tell django to create a new profile when a new user is created.

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.template.context_processors import media
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -123,6 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'Website-Home'
 LOGIN_URL='login'
 
